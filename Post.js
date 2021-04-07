f95683823286d127ccaba284603291a21424e011
wingio <mcprowing@gmail.com>
2021-04-06 22:24:43 -0400

    minor

--- Post.js
+++ Post.js
@@ -10,17 +10,5 @@ module.exports = {
     },
     likes: 100,
     reposts: 5,
-    replies: [{
-        id: 'hufg4ryuwegf6761',
-        content: 'This is a dummy reply',
-        author: {
-            id: 'gyegyuf76645aw56d76a45',
-            username: 'wing',
-            displayName: 'Wing',
-            verified: true,
-            avatar: 'https://avatars.githubusercontent.com/u/44992537?v=1'
-        },
-        likes: 13,
-        reposts: 1
-    }]
+    replies: []
 }
