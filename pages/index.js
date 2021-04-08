6790d4ae9474e085c5a34add6e24e4cd635792cd
wingio <mcprowing@gmail.com>
2021-04-07 21:09:02 -0400

    Added boilerplate signup form

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,27 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import Post from '../components/Post'
-import Navbar from '../components/Navbar'
 
 export default function Home() {
+  function signup(e){
+    e.preventDefault()
+    var data = {
+      username: e.target.form[0].value,
+      email: e.target.form[1].value,
+      password: e.target.form[2].value
+    }
+    e.target.form[3].disabled = true
+    console.log(data)
+  }
   return (
     <div>
-      <Navbar></Navbar>
+      <div className={styles.signup}>
+        <form onSubmit={signup}>
+          <input id='username' type='text'></input>
+          <input id='email' type='text'></input>
+          <input id='password' type='password'>
+          </input><button id='signup' onClick={signup}>Sign Up</button>
+        </form>
+      </div>
     </div>
   )
 }
