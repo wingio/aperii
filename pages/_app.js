6360b5bf109a427c672ff94b5e5e98ca38c4a78e
wingio <mcprowing@gmail.com>
2021-04-07 21:19:13 -0400

    Updated meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div><Head>
+  <meta property="og:title" content="OpenTwitter" />
+  <meta property="og:description" content="Open source replacement for Twitter" />
+  <meta property="og:url" content="https://opentwitter.wingio.xyz/" />
+</Head> <Component {...pageProps} /></div>
 }
 
 export default MyApp
