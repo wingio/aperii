0daa299cea2cf212b1ac4f21dc1756577b5b9739
wingio <mcprowing@gmail.com>
2021-04-14 19:33:25 -0400

    Replaced flask api with express api

---
+++ express api/index.js
@@ -0,0 +1,107 @@
+const MongoClient = require('mongodb').MongoClient;
+const bcrypt = require('bcrypt')
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'aperii';
+const client = new MongoClient(url);
+// Use connect method to connect to the server
+client.connect(function (err) {
+    console.log('Connected successfully to server');
+
+    const db = client.db(dbName);
+    const collection = db.collection('user');
+
+    const express = require('express')
+    const app = express()
+    app.use(express.json())
+
+    async function genToken(chars, length){
+        var token = ''
+        for (let i = 0; i < length; i++){
+            var rn  = Math.floor(Math.random() * chars.length)
+            token += chars.slice(rn, rn + 1)
+        }
+        var user = await collection.findOne({
+            token: token
+        })
+        if(user){
+            return genToken(chars, length)
+        } else {
+            return token
+        }
+    }
+    async function genId(length){
+        var id = ''
+        var chars = "0123456789"
+        for (let i = 0; i < length; i++){
+            var rn  = Math.floor(Math.random() * chars.length)
+            id += chars.slice(rn, rn + 1)
+        }
+        var user = await collection.findOne({
+            id: id
+        })
+        if(user){
+            return genId(length)
+        } else {
+            return id
+        }
+    }
+
+    app.post('/auth/signup', async (req, res) => {
+        const {
+            email,
+            displayName,
+            username,
+            password
+        } = req.body
+        
+        var user = await collection.findOne({
+            username: username
+        })
+        if (user) {res.status(400).send({status: 400,error: 'Username already taken'}); return}
+
+        var usernameRegex = /[A-Za-z_0-9]+/g
+        var passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/g
+        if(username.length < 4 || username.length > 32) {res.status(400).send({status: 400, error: 'Username must be between 4 and 32 characters long'}); return}
+        if(displayName.length < 4 || displayName.length > 64) {res.status(400).send({status: 400, error: 'Display name must be between 4 and 32 characters long'}); return}
+        if(!usernameRegex.test(username)) {res.status(400).send({status: 400, error: 'Username must only contain "A-Z", "a-z", "0-9" and "_"'}); return}
+        if(!passwordRegex.test(password)) {res.status(400).send({status: 400, error: 'Password must be at least 8 characters, contain at least 1 lowercase letter, 1 uppercase letter, a number, and a special character'}); return}
+        var token = await genToken('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_-', 64)
+        bcrypt.hash(password, 10, async (err, hash) => {
+            collection.insertOne({
+                id: await genId(20),
+                email: email,
+                displayName: displayName,
+                username: username,
+                password: hash,
+                token: token
+            }, (err, result) => {
+                res.status(200).send({username: username, token: token})
+            })
+        })
+    })
+
+    app.post('/auth/login', async (req, res) => {
+        const {
+            username,
+            password
+        } = req.body
+        var user = await collection.findOne({
+            username: username
+        } )
+        if(!user) {res.status(400).send({status: 400, error: 'No user found with that username'})}
+        bcrypt.compare(password, user.password,
+            function (err, result) {
+                if (result == true) {
+                    res.send({username: user.username, token: user.token})
+                } else {
+                    res.status(401).send({status: 401, error: 'Incorrect password'})
+                }
+            })
+    })
+
+    app.listen(5000)
+});
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,24 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-
+var prod = false
 export default function Home() {
-  function signup(e){
+  async function signup(e){
     e.preventDefault()
     var data = {
-      username: e.target.form[0].value,
-      email: e.target.form[1].value,
-      password: e.target.form[2].value
+      displayName: e.target.form[0].value,
+      username: e.target.form[1].value,
+      email: e.target.form[2].value,
+      password: e.target.form[3].value
     }
-    e.target.form[3].disabled = true
-    console.log(data)
+    e.target.form[4].disabled = true
+    var res = await fetch('http://127.0.0.1:5000/auth/signup', {
+      method: 'POST',
+      headers: {
+        "content-type": 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    console.log(await res.json())
   }
   return (
     <div>
@@ -18,13 +26,19 @@ export default function Home() {
         <meta property="og:title" content="Home" />
         <title>Home | aperii</title>
       </Head>
-      <div className={styles.signup}>
-        <form onSubmit={signup}>
-          <input id='username' type='text'></input>
-          <input id='email' type='text'></input>
-          <input id='password' type='password'>
-          </input><button id='signup' onClick={signup}>Sign Up</button>
-        </form>
+      <div className={`${styles.signup} forms`}>
+        {prod ? <form className="login-form" onSubmit={signup}>
+          <h1>Sign Up</h1>
+          <label htmlFor="displayname">Display Name</label>
+          <input type="text" name="displayname" id="displayname" placeholder=" " autoComplete="off" className="form-control-material" required />
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" placeholder=" " autoComplete="off" className="form-control-material" required />
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email" placeholder=" " autoComplete="off" className="form-control-material" required />
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" placeholder=" " autoComplete="off" className="form-control-material" required />
+          <button type="submit" className="btn btn-primary btn-ghost" onClick={signup}>Sign Up</button>
+        </form> : ''}
       </div>
     </div>
   )
