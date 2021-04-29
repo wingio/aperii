05995db1f1e93b19eb577214d8d23279106ec667
wingio <mcprowing@gmail.com>
2021-04-28 21:06:41 -0400

    Added debug overlay

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import styles from '../styles/globals.css'
 import Head from 'next/head'
 import Search from '../components/Search'
+import * as info from '../info.json'
 
 function MyApp({ Component, pageProps }) {
   return (<div>
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
       <meta name="description" content="Aperii is an open-source social media designed to function like Twitter, but aims to fix many of it's problems." />
     </Head>
     <Component {...pageProps} />
+    <div style={{position: "fixed", right: "20px", bottom: "20px", color: "white", padding: "1em", background: "#1e262ea1", borderRadius: ".75em", fontSize:"small", lineHeight: "2em"}}>{`Version: ${info.version}`}<br></br>{`API: v${info.apiVersion}`}</div>
   </div>)
 }
 
