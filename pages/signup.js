f15ec175fc00862107837a120787532dc2c9c2ab
wingio <mcprowing@gmail.com>
2021-04-29 20:55:43 -0400

    changed api url

--- pages/signup.js
+++ pages/signup.js
@@ -8,7 +8,7 @@ export default function Home() {
   if (typeof window !== "undefined") {
   var token = localStorage.getItem('token')
   if (token && prod == true) {
-    fetch('https://api.aperii.com/auth/validate', {
+    fetch('https://aperii.com/api/v1/auth/validate', {
       method: 'POST',
       headers: {
         authorization: token
@@ -36,7 +36,7 @@ export default function Home() {
       password: e.target.form[3].value
     }
     e.target.form[4].disabled = true
-    var res = await fetch('https://api.aperii.com/auth/signup', {
+    var res = await fetch('https://aperii.com/api/v1/auth/signup', {
       method: 'POST',
       headers: {
         "content-type": 'application/json'
