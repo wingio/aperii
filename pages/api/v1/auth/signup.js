ece8005b09f0380da80dc49b1c67aeade073c2c7
wingio <mcprowing@gmail.com>
2021-04-29 20:59:05 -0400

    k

--- pages/api/v1/auth/signup.js
+++ pages/api/v1/auth/signup.js
@@ -3,6 +3,7 @@
 export default (req, res) => {
     if(req.method =="POST"){
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+        JSON.stringify(req.body)
         fetch('https://api.aperii.com/auth/signup', {
             method: 'POST',
             body: JSON.stringify(req.body)
