95684621c2d17378bc1384ccd2f82e71627d8e71
wingio <mcprowing@gmail.com>
2021-04-29 20:54:02 -0400

    added auth/signup

---
+++ pages/api/v1/auth/signup.js
@@ -0,0 +1,13 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default (req, res) => {
+    if(req.method =="POST"){
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+        fetch('https://api.aperii.com/auth/signup', {
+            method: 'POST',
+            body: JSON.stringify(req.body)
+        }).then(res => res.json()).then(json => {
+            res.status(json.status).send(json)
+        })
+    }
+}
