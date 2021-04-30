f083072716f675f3dbf4d674a0b106104eddbb30
wingio <mcprowing@gmail.com>
2021-04-29 20:44:32 -0400

    testing api wrapper

--- pages/api/v1/auth/login.js
+++ pages/api/v1/auth/login.js
@@ -2,8 +2,11 @@
 
 export default (req, res) => {
     if(req.method =="POST"){
-        console.log(req)
-        res.status(200).send('ok')
+        fetch('https://api.aperii.com/auth/login', {
+            method: 'POST',
+            body: JSON.stringify(req.body)
+        }).then(res => res.json()).then(json => {
+            res.status(json.status).send(json)
+        })
     }
 }
-  
