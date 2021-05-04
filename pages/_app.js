2d20cb793147c73f94235daa092597e4344c07e2
wingio <mcprowing@gmail.com>
2021-05-04 14:37:41 -0400

    ^

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
       <meta property="og:site_name" content="Aperii" />
       <meta property="og:url" content="https://aperii.wingio.xyz/" />
       <meta itemprop="image" content="public/logo.png" />
-      <meta name="description" content="Aperii is an open-source social media designed to function like Twitter, but aims to fix many of it's problems." />
+      <meta name="description" content="A free, more open social experience" />
     </Head>
     <Component {...pageProps} />
     <div style={{position: "fixed", right: "20px", bottom: "20px", color: "white", padding: "1em", background: "#1e262ea1", borderRadius: ".75em", fontSize:"small", lineHeight: "2em"}}>{`Version: ${info.version}`}<br></br>{`API: v${info.apiVersion}`}</div>
