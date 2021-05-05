40b48cf58806d015a38cc0cc0d557969500b8c97
wingio <mcprowing@gmail.com>
2021-05-05 14:52:01 -0400

    fixed profile page sort

--- components/PostFeed.js
+++ components/PostFeed.js
@@ -2,6 +2,9 @@ import React from 'react'
 import Post from './Post'
 
 export default function PostFeed({ posts }) {
+    posts.sort((a, b) => {
+        return (a.createdTimestamp > b.createdTimestamp) ? -1 : (a.createdTimestamp > b.createdTimestamp) ? 1 : 0 
+    })
     return (
         <div>
             {posts.map(p => <Post data={p} key={p.id}></Post>)}
