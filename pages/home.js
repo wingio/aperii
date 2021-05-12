ea5ce8ebd99272e7e6ae0b26d20df6476a9bbb38
wingio <mcprowing@gmail.com>
2021-05-12 15:00:13 -0400

    another fix

--- pages/home.js
+++ pages/home.js
@@ -56,12 +56,7 @@ export default function Demo( { posts, user } ) {
 }
 
 export async function getServerSideProps(context) {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; = await fetch('https://aperii.com/api/v1/posts/all', {
-    method: 'GET',
-    headers: {
-      authorization: context.req.cookies.token
-    }
-  })
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; 
   var res;
   var userres;
   if (context.req.cookies.token) {
