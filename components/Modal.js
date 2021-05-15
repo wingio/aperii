9fb360c0ca4ca1946cd31e79f1d729da3aa7847b
wingio <mcprowing@gmail.com>
2021-05-15 12:11:31 -0400

    Modal testing

--- components/Modal.js
+++ components/Modal.js
@@ -3,11 +3,16 @@ import styles from '../styles/Modal.module.css'
 import ButtonPrimary from './Button-Primary'
 
 export default function Modal() {
+
+    function testF(e) {
+
+    }
+
     return (
         <div className={styles.background}>
             <div className={styles.popout}>
                 <div className={styles.bottom}>
-                    <ButtonPrimary label="Save"></ButtonPrimary>
+                    <ButtonPrimary label="Save" click={testF}></ButtonPrimary>
                 </div>
             </div>            
         </div>
