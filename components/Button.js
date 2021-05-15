2de946f959a91e30d365397e5893f25551f2c7e4
wingio <mcprowing@gmail.com>
2021-05-15 18:32:18 -0400

    Modal Testing

--- components/Button.js
+++ components/Button.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '../styles/Button-Primary.module.css'
+import styles from '../styles/Button.module.css'
 
 export default function Button({ label, click, style }) {
     return (
