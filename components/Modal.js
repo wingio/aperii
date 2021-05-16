219e2a5bbbe8ea4bb03714d1894ac3b8cee450b5
wingio <mcprowing@gmail.com>
2021-05-15 20:47:53 -0400

    modal testing

--- components/Modal.js
+++ components/Modal.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from '../styles/Modal.module.css'
 import Button from './Button'
 
 
 export default function Modal({ children, title, subtitle }) {
 
+    const [isOpened, setOpened] = useState(true)
+
     function testF(e) {
 
     }
 
+    function closeModal(e) {
+        setOpened(false)
+    }
+
+
+
     return (
-        <div className={styles.background}>
-            <div className={styles.popout}>
-                <div className={styles.content}>
-                    <h3 style={{marginTop: 0, color: "white", marginBottom: subtitle ? 0 : 'revert'}}>{title ? title : 'Hello 👋'}</h3>
-                    {subtitle ? <p style={{marginTop: ".2em"}}>{subtitle}</p> : ''}
-                    {children}
-                </div>
-                <div className={styles.bottom}>
-                    <Button label="Dismiss" click={testF} style="secondary"></Button>
-                    <Button label="Save" click={testF} style="primary"></Button>
-                </div>
-            </div>            
-        </div>
+        isOpened ? 
+        <div className={styles.background} onClick={closeModal}>
+        <div className={styles.popout}>
+            <div className={styles.content}>
+                <h3 style={{marginTop: 0, color: "white", marginBottom: subtitle ? 0 : 'revert'}}>{title ? title : 'Hello 👋'}</h3>
+                {subtitle ? <p style={{marginTop: ".2em", color: "white"}}>{subtitle}</p> : ''}
+                {children}
+            </div>
+            <div className={styles.bottom}>
+                <Button label="Dismiss" click={testF} style="secondary"></Button>
+                <Button label="Save" click={closeModal} style="primary"></Button>
+            </div>
+        </div>            
+    </div> : <></>
     )
 }
