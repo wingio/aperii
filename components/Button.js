5aac152ea30a320b80d9265de60d8fb0ee2aa66a
wingio <mcprowing@gmail.com>
2021-05-15 21:04:59 -0400

    modal test

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from '../styles/Button.module.css'
-
-export default function Button({ label, click, style }) {
+/**
+ * 
+ * @param {React.ButtonHTMLAttributes<HTMLButtonElement>} props
+ * @returns 
+ */
+export default function Button(props) {
+    const { label, click, style } = props
     return (
-        <button className={styles.btnPrimary} className={styles[style] ? styles[style] : styles.primary}>
+        <button className={styles.btnPrimary} className={styles[style] ? styles[style] : styles.primary} {...props}>
             {label}
         </button>
     )
