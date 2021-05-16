b9c7ddd7b03c722a224a3d87394a1697b51f9b46
wingio <mcprowing@gmail.com>
2021-05-16 14:52:01 -0400

    modal test

--- components/Changelog.js
+++ components/Changelog.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import Modal from './Modal'
 import SectionTitle from './SectionTitle'
 import * as info from '../info.json'
+import styles from '../styles/Changelog.module.css'
 export default function Changelog() {
     const [opened, setOpen] = useState(true)
 
@@ -12,7 +13,7 @@ export default function Changelog() {
         opened ? <Modal title="See what's new!" subtitle={`Version: ${info.version}`} buttons={[{label: 'OK', btnstyle: 'primary', onClick: close}]}>
             {info.changelog.image && <img src={info.changelog.image} style={{width: "100%"}}></img>}
             {info.changelog.sections.map(s => 
-                <div key={s.name}><h4 style={{color: "white"}}>{s.name}</h4><p style={{color: "white"}}>{s.value}</p></div>
+                <div key={s.name}><h4 style={{color: "white", display:"flex"}} className={styles.section}>{s.name}</h4><p style={{color: "white"}}>{s.value}</p></div>
             )}
         </Modal> : <></>
     )
