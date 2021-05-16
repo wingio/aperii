cdceed97abe1ac6b50aa6440b6beeed9580cca6b
wingio <mcprowing@gmail.com>
2021-05-16 13:36:41 -0400

    modal test

--- components/Changelog.js
+++ components/Changelog.js
@@ -12,10 +12,10 @@ export default function Changelog() {
         opened ? <Modal title="See what's new!" subtitle={`Version: ${info.version}`} buttons={[{label: 'OK', btnstyle: 'primary', onClick: close}]}>
             {info.changelog.image && <img src={info.changelog.image}></img>}
             {info.changelog.sections.map(s => {
-                <>
+                <div>
                     <SectionTitle style={{color: "white"}}>{s.name}</SectionTitle>
                     <p style={{color: "white"}}>{s.value}</p>
-                </>
+                </div>
             })}
         </Modal> : <></>
     )
