4828e6a21e5b1b9f1146c9e43b7f768465c06da2
wingio <mcprowing@gmail.com>
2021-05-16 15:52:37 -0400

    post button test

--- components/MobilePostBtn.js
+++ components/MobilePostBtn.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import PostIcon from '../icons/CreatePost'
 export default function MobilePostBtn({user}) {
     return (
-        <button style={{border: "none", padding: "0", position: "fixed", bottom: "0.5em", right: "0.5em", borderRadius: "50%", background: "var(--rust-blood)", width:"4rem", height:"4rem", fontSize: "3rem", fontWeight: "bolder", margin: "auto"}}>
-            +
+        <button style={{border: "none", padding: "0", position: "fixed", bottom: "0.5em", right: "0.5em", borderRadius: "50%", background: "var(--rust-blood)", width:"4rem", height:"4rem", fontSize: "3rem", fontWeight: "bolder", margin: "auto", color: "white"}}>
+            <PostIcon width="3rem"/>
         </button>
     )
 }
