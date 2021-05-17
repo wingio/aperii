f2aa928a644bc5be5c91df98adf1e71533d15b70
wingio <mcprowing@gmail.com>
2021-05-17 16:53:16 -0400

    testing

--- components/ExperimentsModal.js
+++ components/ExperimentsModal.js
@@ -4,7 +4,7 @@ import TextBox from './TextBox'
 import ModalForm from './ModalForm'
 export default function ExperimentsModal({ user, closeAction, exp }) {
     var close = closeAction
-
+    const [value, setValue] = useState(exp)
     var update = (e) => {
         console.log(e.target.value)
         exp[e.target.name] = e.target.value
@@ -22,7 +22,7 @@ export default function ExperimentsModal({ user, closeAction, exp }) {
         <Modal title="Feeling like a scientist?" subtitle="Try out new features before they're public" buttons={[{label: 'Dismiss', btnstyle: 'secondary', onClick: close}, {label: 'Save', btnstyle: 'primary', onClick: save}]}>
                 <p style={{marginBottom: "0.2em"}}>New line support for textboxes</p>
                 <p style={{fontSize: "0.7em", opacity: 0.5, marginTop:0}}>textbox_newline_05_17_21</p>
-                <select name="textbox_newline_05_17_21" id="exp1" onChange={update} value={exp["textbox_newline_05_17_21"] ? exp["textbox_newline_05_17_21"] : "0"}>
+                <select name="textbox_newline_05_17_21" id="exp1" onChange={update} value={value["textbox_newline_05_17_21"] ? value["textbox_newline_05_17_21"] : "0"}>
                     <option value="0">Control</option>
                     <option value="1">Treatment 1</option>
                 </select>
