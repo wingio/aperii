fce747263ac5d6a18485845d658e346d29980b21
wingio <mcprowing@gmail.com>
2021-05-17 16:35:11 -0400

    testing

--- components/ExperimentsModal.js
+++ components/ExperimentsModal.js
@@ -4,33 +4,20 @@ import TextBox from './TextBox'
 import ModalForm from './ModalForm'
 export default function ExperimentsModal({ user, closeAction, exp }) {
     var close = closeAction
+    const [values, setValues] = useState('')
 
     var save = (e) => {
-        e.preventDefault()
-
         console.log(e)
-
-        const experiments = [
-            "textbox_newline_05_17_21"
-        ]
-        
-        experiments.forEach((e, i) => {
-            console.log(e.target)
-            //console.log(e.target.form[i].value)
-        })
-
     }
 
     return (
-        <Modal title="Feeling like a scientist?" subtitle="Try out new features before they're public" buttons={[{label: 'Dismiss', btnstyle: 'secondary', onClick: close}, {label: 'Save', btnstyle: 'primary', form: "modal-exp", onClick: save}]}>
-            <ModalForm id="modal-exp">
+        <Modal title="Feeling like a scientist?" subtitle="Try out new features before they're public" buttons={[{label: 'Dismiss', btnstyle: 'secondary', onClick: close}, {label: 'Save', btnstyle: 'primary', onClick: save}]}>
                 <p style={{marginBottom: "0.2em"}}>New line support for textboxes</p>
                 <p style={{fontSize: "0.7em", opacity: 0.5, marginTop:0}}>textbox_newline_05_17_21</p>
-                <select name="textbox_newline_05_17_21" id="exp1">
+                <select name="textbox_newline_05_17_21" id="exp1" onChange={save}>
                     <option value="0">Control</option>
                     <option value="1">Treatment 1</option>
                 </select>
-            </ModalForm>
         </Modal>
     )
 }
