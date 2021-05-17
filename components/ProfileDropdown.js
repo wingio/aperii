d8a2ce6efa12a57cae09da3603b6cd19c1ea37e3
wingio <mcprowing@gmail.com>
2021-05-17 14:05:09 -0400

    :)

--- components/ProfileDropdown.js
+++ components/ProfileDropdown.js
@@ -1,18 +1,28 @@
+import { faCloudShowersHeavy } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import Dropdown from './Dropdown'
 import DropdownItem from './DropdownItem'
 
 function ProfileDropdown(props) {
-    return (
-        <Dropdown>
-          <DropdownItem loc={`/p/${props.user.username}`} label="Profile" icon='user'></DropdownItem>
-          <DropdownItem label="Report a bug" icon='bug' loc="https://github.com/wingio/aperii/issues/new/choose"></DropdownItem>
-          <DropdownItem label="Language" icon='globe' loc="/"></DropdownItem>
-          <DropdownItem label="Discord" icon='discord' loc="https://discord.gg/Mryxr7zVtc"></DropdownItem>
-          <DropdownItem label="Settings" icon='gear' loc="/"></DropdownItem>
-          <DropdownItem label="Log Out" icon='gear' action="logout"></DropdownItem>
-        </Dropdown>
-    );
+  var expiramentsEnabled = faCloudShowersHeavy
+  if (typeof window !== "undefined") {
+    var exp = localStorage.getItem('enableExpirements')
+    if (exp) {
+      exp == true || exp == "true" ? expiramentsEnabled = true : expiramentsEnabled = false
+    }
+  }
+
+  return (
+      <Dropdown>
+        <DropdownItem loc={`/p/${props.user.username}`} label="Profile" icon='user'></DropdownItem>
+        <DropdownItem label="Report a bug" icon='bug' loc="https://github.com/wingio/aperii/issues/new/choose"></DropdownItem>
+        <DropdownItem label="Language" icon='globe' loc="/"></DropdownItem>
+        <DropdownItem label="Discord" icon='discord' loc="https://discord.gg/Mryxr7zVtc"></DropdownItem>
+        <DropdownItem label="Settings" icon='gear' loc="/"></DropdownItem>
+        {expiramentsEnabled ? <DropdownItem label="Experiments" icon='gear' loc="/"></DropdownItem> : ''}
+        <DropdownItem label="Log Out" icon='gear' action="logout"></DropdownItem>
+      </Dropdown>
+  );
 }
 
 export default ProfileDropdown;
