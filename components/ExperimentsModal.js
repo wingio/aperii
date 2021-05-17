0ce3b2b9a0fc76689661f8421be978f6fec508c8
wingio <mcprowing@gmail.com>
2021-05-17 15:36:45 -0400

    fix

--- components/ExperimentsModal.js
+++ components/ExperimentsModal.js
@@ -11,7 +11,7 @@ export default function ExperimentsModal({ user, closeAction }) {
 
     return (
         <Modal title="Feeling like a scientist?" subtitle="Try out new features before they're public" buttons={[{label: 'Dismiss', btnstyle: 'secondary', onClick: close}, {label: 'Save', btnstyle: 'primary', form: "modal-exp", onClick: save}]}>
-            <ModalForm onSubmit={post} id="modal-exp">
+            <ModalForm id="modal-exp">
                 <p style={{marginBottom: "0.2em"}}>New line support for textboxes</p>
                 <p style={{fontSize: "0.7em", opacity: 0.5, marginTop:0}}>textbox_newline_05_17_21</p>
                 <select name="experiment1" id="exp1">
