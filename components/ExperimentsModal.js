8caac8e1bd7831e477f01b52611cd28446417fb5
wingio <mcprowing@gmail.com>
2021-05-17 16:49:52 -0400

    finally working :)

--- components/ExperimentsModal.js
+++ components/ExperimentsModal.js
@@ -22,7 +22,7 @@ export default function ExperimentsModal({ user, closeAction, exp }) {
         <Modal title="Feeling like a scientist?" subtitle="Try out new features before they're public" buttons={[{label: 'Dismiss', btnstyle: 'secondary', onClick: close}, {label: 'Save', btnstyle: 'primary', onClick: save}]}>
                 <p style={{marginBottom: "0.2em"}}>New line support for textboxes</p>
                 <p style={{fontSize: "0.7em", opacity: 0.5, marginTop:0}}>textbox_newline_05_17_21</p>
-                <select name="textbox_newline_05_17_21" id="exp1" onChange={update}>
+                <select name="textbox_newline_05_17_21" id="exp1" onChange={update} value={exp["textbox_newline_05_17_21"] ? exp["textbox_newline_05_17_21"] : "0"}>
                     <option value="0">Control</option>
                     <option value="1">Treatment 1</option>
                 </select>
