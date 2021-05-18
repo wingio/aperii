bb23c7d80f68b535bee956452da33985b1d2bf46
wingio <mcprowing@gmail.com>
2021-05-17 21:10:37 -0400

    remove a console log

--- components/PostBody.js
+++ components/PostBody.js
@@ -3,8 +3,7 @@ const tokenize = require('../tokenizer')
 
 export default function PostBody({ text }) {
 
-    var tokens = tokenize(text.replace('&nbsp;', ''))
-    console.log(tokens)
+    var tokens = tokenize(text.replace(/&nbsp;/g, ''))
     return (
         <>
             {tokens.map((tok, i) => tok.type == 0 ? `${tok.value}` : tok.type == 1 ? <a href={`/p/${tok.value}`} style={{marginRight: '.2em', marginLeft: `${i == 0 ? '0em' : '.2em'}`}}>@{tok.value}</a> : tok.type = 3 ? <br /> : '')}
