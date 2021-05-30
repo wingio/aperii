0eedb395fd81178f764757567178edf68ddd4e35
wingio <mcprowing@gmail.com>
2021-05-30 15:26:32 -0400

    test

--- components/EditProfileModal.js
+++ components/EditProfileModal.js
@@ -51,16 +51,14 @@ export default function MakePostModal({ user, closeAction }) {
         setSource(await toBase64(e.target.files[0]))
     }
 
-    const [inputVisible, setVis] = useState(false)
-
     return (
         opened ? <Modal title="What's on your mind?" subtitle="To let everyone know, make a post!" buttons={[{label: 'Dismiss', btnstyle: 'secondary', onClick: close}, {label: 'Post', btnstyle: 'primary', form: "modal-postform", onClick: post}]}>
             <ModalForm onSubmit={post} id="modal-postform">
-                <label className={styles.avSelect} style={{display: inputVisible == false ? 'none' : 'revert'}}>
+                <label className={styles.avSelect}>
                     <input type="file" accept=".png, .jpg, .jpeg, .gif" multiple={false} onChange={updatePreview} style={{display: "hidden"}}/>
                     Select Image
                 </label>
-                <img src={source} width="100px" height="100px" style={{borderRadius: "50%"}} onMouseEnter={() => {setVis(true)}} onMouseLeave={() => {setVis(false)}}></img>
+                <img src={source} width="100px" height="100px" style={{borderRadius: "50%"}}></img>
             </ModalForm>
         </Modal> : <></>
     )
