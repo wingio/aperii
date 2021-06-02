b5b85efb8ef2aba72cd9c2b4ac31278e54548162
wingio <mcprowing@gmail.com>
2021-06-02 14:59:46 -0400

    fix build error

--- components/VerifiedInfoModal.js
+++ components/VerifiedInfoModal.js
@@ -3,7 +3,7 @@ import Modal from './Modal'
 import Icon from '../icons/Icon'
 export default function VerifiedInfoModal({close, vanished}) {
     return (
-        <Modal title="This acount is verified" buttons={[{label: 'OK', btnstyle: 'primary', onClick: close}]} showVanish={}>
+        <Modal title="This acount is verified" buttons={[{label: 'OK', btnstyle: 'primary', onClick: close}]} showVanish={vanished}>
             <Icon name="badge" width="1.4rem"></Icon>
             This account is verified because it belongs to a company, influential figure or entertainer, or is an offical Aperii account.
         </Modal>
