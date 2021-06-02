b5b074d440cb87e5c01ab65e564657f9d39c38ff
wingio <mcprowing@gmail.com>
2021-06-02 15:14:13 -0400

    fixed styles not applying

--- components/VerifiedInfoModal.js
+++ components/VerifiedInfoModal.js
@@ -5,7 +5,7 @@ import styles from '../styles/VerifiedInfoModal.module.css'
 export default function VerifiedInfoModal({close, vanished}) {
     return (
         <Modal buttons={[{label: 'OK', btnstyle: 'primary', onClick: close}]} showVanish={vanished}>
-            <section>
+            <section className={styles.section}>
                 <Icon name="badge" width="3rem" style={{marginBottom: "10px"}}></Icon>
                 This account is verified because it belongs to a company, influential figure, or is an offical Aperii account.
             </section>
