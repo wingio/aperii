c06783e545045b57a295ad4e8eb83728aca6c5e4
wingio <mcprowing@gmail.com>
2021-06-02 15:03:26 -0400

    styling

--- components/VerifiedInfoModal.js
+++ components/VerifiedInfoModal.js
@@ -4,8 +4,10 @@ import Icon from '../icons/Icon'
 export default function VerifiedInfoModal({close, vanished}) {
     return (
         <Modal title="This acount is verified" buttons={[{label: 'OK', btnstyle: 'primary', onClick: close}]} showVanish={vanished}>
-            <Icon name="badge" width="1.4rem"></Icon>
-            This account is verified because it belongs to a company, influential figure or entertainer, or is an offical Aperii account.
+            <section>
+                <Icon name="badge" width="1.4rem"></Icon>
+                This account is verified because it belongs to a company, influential figure or entertainer, or is an offical Aperii account.
+            </section>
         </Modal>
     )
 }
