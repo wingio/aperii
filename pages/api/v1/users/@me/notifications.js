54c51cf5cff2b75179063fdae7ca252a3646f7f2
wingio <mcprowing@gmail.com>
2021-06-11 15:16:28 -0400

    added /users/@me/notification route

---
+++ pages/api/v1/users/@me/notifications.js
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+
+export default (req, res) => {
+    if(req.method =="GET"){
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+        //console.log(req)
+        //JSON.stringify(req.body)
+        fetch(`https://api.aperii.com/users/me/notifications`, {
+            method: 'GET',
+            body: JSON.stringify(req.body),
+            headers: {
+                'content-type': 'application/json',
+                authorization: req.headers.authorization
+            }
+        }).then(res => res.json()).then(json => {
+            res.status(json.status ? json.status : 200).send(json)
+        })
+    }
+}
