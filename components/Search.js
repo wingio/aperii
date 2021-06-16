26a3bfe7186314d8648e5c5f08f72cb69afd95f0
wingio <mcprowing@gmail.com>
2021-06-16 14:48:39 -0400

    test

--- components/Search.js
+++ components/Search.js
@@ -5,7 +5,7 @@ import Link from 'next/link'
 const Search = (props) => {
     return (<form className={`search-container${props.mobile ? '-mobile' : ''} sticky`}>
         {/* <input type='text' placeholder="Search..." className={style.search}></input> */}
-        <b>Page Title</b>
+        <h3 style={{marginLeft: "1em", color:"white"}}>Page Title</h3>
     </form>)
 }
 
