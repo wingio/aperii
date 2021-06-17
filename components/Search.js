f3b30796ebd3935a3bb10eff8204122ca4ae5e87
wingio <mcprowing@gmail.com>
2021-06-17 14:36:28 -0400

    move postcount

--- components/Search.js
+++ components/Search.js
@@ -3,11 +3,12 @@ import Link from 'next/link'
 import Icon from '../icons/Icon'
 
 const Search = (props) => {
-    return (<form className={`search-container${props.mobile ? '-mobile' : ''} sticky`}>
-        {/* <input type='text' placeholder="Search..." className={style.search}></input> */}
-        <h3 style={{margin: 0, marginLeft: "1em", color:"white"}}>{props.title} {props.showBadge ? <Icon name="badge" width="1.1rem" style={{marginBottom: "-.1em"}} /> : ''}</h3>
-        {props.showPosts ? <p style={{margin: 0, color: "#888"}}>{props.postCount} posts</p>: ''}
-    </form>)
+    return (<div className={`search-container${props.mobile ? '-mobile' : ''} sticky`}>
+        <div>
+            <h3 style={{margin: 0, marginLeft: "1em", color:"white"}}>{props.title} {props.showBadge ? <Icon name="badge" width="1.1rem" style={{marginBottom: "-.1em"}} /> : ''}</h3>
+            {props.showPosts ? <p style={{margin: 0, color: "#888", marginLeft: "20px"}}>{props.postCount} posts</p>: ''}
+        </div>
+    </div>)
 }
 
 export default Search;
