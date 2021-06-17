1bf35f301974bda494da60f89d0b24ea5cf4112f
wingio <mcprowing@gmail.com>
2021-06-17 13:38:53 -0400

    fix crash + testing

--- components/ContextMenu.js
+++ components/ContextMenu.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 export default function ContextMenu() {
     return (
-        <menu style={{color: 'white'}}>
-            <menuitem>Like Post</menuitem>
-            <menuitem>Share Post</menuitem>
-        </menu>
+        <div style={{color: "white"}}>
+            <ul>
+                <li>Like post</li>
+                <li>Share post</li>
+            </ul>
+        </div>
     )
 }
