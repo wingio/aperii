50acf9513a6f8a752d0ff609db3fd309232fcaa9
wingio <mcprowing@gmail.com>
2021-06-17 13:34:50 -0400

    test

---
+++ components/ContextMenu.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ContextMenu() {
+    return (
+        <menu style={{color: 'white'}}>
+            <menuitem>Like Post</menuitem>
+            <menuitem>Share Post</menuitem>
+        </menu>
+    )
+}
--- components/Post.js
+++ components/Post.js
@@ -3,20 +3,20 @@ import Tooltip from './Tooltip'
 import {useState} from 'react'
 import PostBody from './PostBody'
 import Badge from '../icons/Badge'
+import ContextMenu from './ContextMenu'
+
 const data = ({data, embed}) => {
     const [visible, setVisible] = useState(false)
 
-    function showTooltip() {
-        setVisible(true)
-    }
-
-    function hideTooltip() {
-        setVisible(false)
+    function openContext(e){
+        e.preventDefault()
+        console.log(e)
+        setVisible(!visible)
     }
 
     var umentionRegex = /@[a-zA-Z0-9_]+/g
 
-    return (<div className={postStyle.post + ` ${embed ? postStyle.embed : ''}`}>
+    return (<div className={postStyle.post + ` ${embed ? postStyle.embed : ''}`} onContextMenu={openContext}>
         <div className={postStyle.avcontainer}>
             <img className={postStyle.av} src={data.author.avatar ? data.author.avatar : '/av.png'}></img>
         </div>
@@ -28,6 +28,7 @@ const data = ({data, embed}) => {
         </div>
             <p className={postStyle.content}><PostBody text={data.body}></PostBody></p>
         </div>
+        {visible ? <ContextMenu></ContextMenu> : ''}
     </div>)
 }
 
