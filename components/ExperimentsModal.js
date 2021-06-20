845a6e448f698fbb7871e288f46d7374e963d1ae
wingio <mcprowing@gmail.com>
2021-06-20 13:19:31 -0400

    misc colors

--- components/ExperimentsModal.js
+++ components/ExperimentsModal.js
@@ -21,20 +21,20 @@ export default function ExperimentsModal({ user, closeAction, exp }) {
 
     return (
         <Modal title="Feeling like a scientist?" subtitle="Try out new features before they're public" buttons={[{label: 'Dismiss', btnstyle: 'secondary', onClick: close}, {label: 'Save', btnstyle: 'primary', onClick: save}]}>
-                <p style={{marginBottom: "0.2em"}}>Notification tab</p>
-                <p style={{fontSize: "0.7em", opacity: 0.5, marginTop:0}}>noti_tab_06_05_21</p>
+                <p style={{marginBottom: "0.2em", color: "var(--text-color)"}}>Notification tab</p>
+                <p style={{fontSize: "0.7em", opacity: 0.5, marginTop:0, color: "var(--text-color)"}}>noti_tab_06_05_21</p>
                 <select name="noti_tab_06_05_21" id="exp2" onChange={update} defaultValue={value["noti_tab_06_05_21"] ? value["noti_tab_06_05_21"] : "0"}>
                     <option value="0">Control</option>
                     <option value="1">Treatment 1</option>
                 </select>
-                <p style={{marginBottom: "0.2em"}}>New mobile layout</p>
-                <p style={{fontSize: "0.7em", opacity: 0.5, marginTop:0}}>mobile_layout_05_18_21</p>
+                <p style={{marginBottom: "0.2em", color: "var(--text-color)"}}>New mobile layout</p>
+                <p style={{fontSize: "0.7em", opacity: 0.5, marginTop:0, color: "var(--text-color)"}}>mobile_layout_05_18_21</p>
                 <select name="mobile_layout_05_18_21" id="exp2" onChange={update} defaultValue={value["mobile_layout_05_18_21"] ? value["mobile_layout_05_18_21"] : "0"}>
                     <option value="0">Control</option>
                     <option value="1">Treatment 1</option>
                 </select>
-                <p style={{marginBottom: "0.2em"}}>New line support for textboxes</p>
-                <p style={{fontSize: "0.7em", opacity: 0.5, marginTop:0}}>textbox_newline_05_17_21</p>
+                <p style={{marginBottom: "0.2em", color: "var(--text-color)"}}>New line support for textboxes</p>
+                <p style={{fontSize: "0.7em", opacity: 0.5, marginTop:0, color: "var(--text-color)"}}>textbox_newline_05_17_21</p>
                 <select name="textbox_newline_05_17_21" id="exp1" onChange={update} defaultValue={value["textbox_newline_05_17_21"] ? value["textbox_newline_05_17_21"] : "0"}>
                     <option value="0">Control</option>
                     <option value="1">Treatment 1</option>
