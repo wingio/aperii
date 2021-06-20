6fb31cae962423c6fbf0ec6a1a725cf1b1c04d9c
wingio <mcprowing@gmail.com>
2021-06-19 20:50:54 -0400

    tab bar icons

--- components/SidebarFeedOption.js
+++ components/SidebarFeedOption.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Icon from '../icons/Icon'
 
 export default function SidebarFeedOption( { name, current, icon } ) {
     return (
         <div className={`sidebar-option ${current ? 'current' : ''}`}>
-            <img src={icon ? `/icons/${icon}.svg` : ''}></img>
+            <Icon name={icon} width="20px" style={{color: current ? "white" : "var(--icon-color)"}} />
             <p>{name}</p>
         </div>
     )
