d60fdfd09e1a1366d88811e6e47e9bd5341916a4
wingio <mcprowing@gmail.com>
2021-06-20 14:47:07 -0400

    proper theme icon rendered

--- components/ProfileDropdown.js
+++ components/ProfileDropdown.js
@@ -13,16 +13,26 @@ function ProfileDropdown(props) {
   }
 
   const [modalOpened, setOpened] = useState(false)
-    var close = () => {
-        setOpened(false)
+  var close = () => {
+    setOpened(false)
+  }
+
+  const [themeIcon, setThemeIcon] = useState('sun')
+  if (typeof window != "undefined") {
+    var theme = localStorage.getItem('theme') ? localStorage.getItem('theme') : "dark"
+    if (theme == "light") {
+      setThemeIcon('moon')
+    } else {
+      setThemeIcon('sun')
     }
+  }
 
   return (
       <Dropdown>
         <DropdownItem label="Report a bug" icon='bug' loc="https://github.com/wingio/aperii/issues/new/choose"></DropdownItem>
         <DropdownItem label="Language" icon='globe' loc="/"></DropdownItem>
         <DropdownItem label="Discord" icon='discord' loc="https://discord.gg/Mryxr7zVtc"></DropdownItem>
-        <DropdownItem label="Toggle theme" icon="moon" action="toggletheme" />
+        <DropdownItem label="Toggle theme" icon={themeIcon} action="toggletheme" />
         <DropdownItem label="Settings" icon='gear' loc="/settings"></DropdownItem>
         {expiramentsEnabled ? <DropdownItem label="Experiments" icon='gear' click={() => {setOpened(true)}}></DropdownItem> : ''}
         <DropdownItem label="Log Out" icon='gear' action="logout"></DropdownItem>
