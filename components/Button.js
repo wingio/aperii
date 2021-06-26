dc2c05b13c293de6038953623466ce599fcf52ba
wingio <mcprowing@gmail.com>
2021-06-25 20:42:48 -0400

    the actual fix

--- components/Button.js
+++ components/Button.js
@@ -8,7 +8,7 @@ import Loading from '../icons/Loading'
  */
 export default function Button(props) {
     const { label, btnstyle, loading } = props
-    var disabled = (typeof loading != "undefined") ? !!loading : props.disabled
+    var disabled = (typeof props.loading != "undefined") ? props.disabled : !!loading
     return (
         <button className={styles[btnstyle] ? styles[btnstyle] : styles.primary} {...props} disabled={disabled}>
             {loading ? <Loading style={{color: "rgba(255, 255, 255, .7)"}} height="8px" /> : label}
