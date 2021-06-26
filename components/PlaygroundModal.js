8ec68d39a58c56bbd5a81e866b967154d0084392
wingio <mcprowing@gmail.com>
2021-06-26 10:50:48 -0400

    fix

--- components/PlaygroundModal.js
+++ components/PlaygroundModal.js
@@ -8,11 +8,11 @@ export default function PlaygroundModal({ user, closeAction, exp }) {
     var close = closeAction
 
     return (
-        <Modal title="UI Playground" subtitle="Explore and test ui elements" buttons={[{label: 'OK', btnstyle: 'primary', onClick: close}]}>
-                <Button label="Primary" btnstyle="primary" style={{marginRight: "10px"}}></Button>
+        <Modal title="UI Playground" subtitle="Explore and test ui elements" buttons={[{label: 'OK', btnstyle: 'primary', onClick: close, disabled: false}]}>
+                <Button label="Primary" btnstyle="primary" style={{marginRight: "10px", marginBottom: "10px"}} disabled={false}></Button>
                 <Button label="Primary Disabled" btnstyle="primary" disabled={true} style={{marginRight: "10px"}}></Button>
                 <Button label="Loading" btnstyle="primary" loading={true} style={{marginRight: "10px"}}></Button>
-                <Button label="Secondary" btnstyle="secondary"></Button>
+                <Button label="Secondary" btnstyle="secondary" disabled={false}></Button>
                 <p>Regular Textbox</p>
                 <TextBox></TextBox>
                 <p>Rich Textbox</p>
