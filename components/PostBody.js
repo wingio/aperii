e3adc7aa552d697a6fb4fe4df733c06741059f33
wingio <mcprowing@gmail.com>
2021-06-28 18:27:41 -0400

    removed twemoji experiment

--- components/PostBody.js
+++ components/PostBody.js
@@ -5,15 +5,9 @@ import Twemoji from 'react-twemoji';
 export default function PostBody({ text, useTwemoji }) {
 
     var tokens = tokenize(text.replace(/&nbsp;/g, ''))
-    if(useTwemoji){
-        return (<Twemoji>
-        {tokens.map((tok, i) => tok.type == 0 ? `${tok.value}` : tok.type == 1 ? <a href={`/p/${tok.value.slice(1)}`}>{tok.value}</a> : tok.type == 3 ? <br /> : '')}
-        </Twemoji>)
-    } else {
-        return (
-            <>
-                {tokens.map((tok, i) => tok.type == 0 ? `${tok.value}` : tok.type == 1 ? <a href={`/p/${tok.value.slice(1).toLowerCase()}`}>{tok.value}</a> : tok.type == 3 ? <br /> : '')}
-            </>
-        )
-    }
+    
+    return (<Twemoji>
+    {tokens.map((tok, i) => tok.type == 0 ? `${tok.value}` : tok.type == 1 ? <a href={`/p/${tok.value.slice(1)}`}>{tok.value}</a> : tok.type == 3 ? <br /> : '')}
+    </Twemoji>)
+    
 }
