7b99d2071cf66762ef3697cb9b338e75290f435e
wingio <mcprowing@gmail.com>
2021-07-01 22:29:44 -0400

    add loading indicator to post modal

--- components/MakePostModal.js
+++ components/MakePostModal.js
@@ -2,13 +2,16 @@ import React, {useState} from 'react'
 import Modal from './Modal'
 import TextBox from './TextBox'
 import ModalForm from './ModalForm'
+import { useRouter } from 'next/router'
 export default function MakePostModal({ user, closeAction, showVanish }) {
     const [opened, setOpen] = useState(true)
-
+    const [loading, setLoading] = useState(false)
+    const router = useRouter()
     var close = closeAction
 
     var post = (e) => {
         e.preventDefault()
+        setLoading(true)
         var body = e.type == "click" ? e.target.form[0].value : e.target[0].value
 
         fetch(`https://aperii.com/api/v1/users/${user.id}/posts`, {
@@ -21,13 +24,14 @@ export default function MakePostModal({ user, closeAction, showVanish }) {
             },
             method: 'POST'
         }).then(res => res.json()).then(json => {
+            setLoading(false)
             close()
-            window.location = '/home'
+            router.prefetch('/home')
         })
     }
 
     return (
-        opened ? <Modal title="What's on your mind?" subtitle="To let everyone know, make a post!" buttons={[{label: 'Dismiss', btnstyle: 'secondary', onClick: close}, {label: 'Post', btnstyle: 'primary', form: "modal-postform", onClick: post}]} showVanish={showVanish}>
+        opened ? <Modal title="What's on your mind?" subtitle="To let everyone know, make a post!" buttons={[{label: 'Dismiss', btnstyle: 'secondary', onClick: close}, {label: 'Post', btnstyle: 'primary', form: "modal-postform", onClick: post, loading: loading}]} showVanish={showVanish}>
             <ModalForm onSubmit={post} id="modal-postform">
                 <TextBox></TextBox>
             </ModalForm>
