70669f1e1657f517e1aaa92947c11b0ec1641578
wingio <mcprowing@gmail.com>
2021-07-22 19:53:10 -0400

    debugging

--- pages/api/v1/users/[id]/posts.js
+++ pages/api/v1/users/[id]/posts.js
@@ -9,17 +9,18 @@ export default (req, res) => {
     if (req.query.replyto) optionalqp.replyto = req.query.replyto
     if(req.method =="POST"){
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+        console.log(`https://api.aperii.com/users/${req.query.id}/posts${Object.keys(optionalqp).length > 1 ? '?' + new URLSearchParams(optionalqp) : ''}`)
         //console.log(req)
         //JSON.stringify(req.body)
-        fetch(`https://api.aperii.com/users/${req.query.id}/posts${Object.keys(optionalqp).length > 1 ? '?' + new URLSearchParams(optionalqp) : ''}`, {
-            method: 'POST',
-            body: JSON.stringify(req.body),
-            headers: {
-                'content-type': 'application/json',
-                authorization: req.headers.authorization
-            }
-        }).then(res => res.json()).then(json => {
-            res.status(json.status ? json.status : 200).send(json)
-        })
+        // fetch(`https://api.aperii.com/users/${req.query.id}/posts${Object.keys(optionalqp).length > 1 ? '?' + new URLSearchParams(optionalqp) : ''}`, {
+        //     method: 'POST',
+        //     body: JSON.stringify(req.body),
+        //     headers: {
+        //         'content-type': 'application/json',
+        //         authorization: req.headers.authorization
+        //     }
+        // }).then(res => res.json()).then(json => {
+        //     res.status(json.status ? json.status : 200).send(json)
+        // })
     }
 }
