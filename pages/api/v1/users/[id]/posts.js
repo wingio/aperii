f62dcc448694cf0e4efdf892d67e9f81b34232f8
wingio <mcprowing@gmail.com>
2021-07-22 19:47:17 -0400

    post route query params

--- pages/api/v1/users/[id]/posts.js
+++ pages/api/v1/users/[id]/posts.js
@@ -1,11 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+import {URLSearchParams} from 'url'
+/**
+ * @param {import("next").NextApiRequest} req
+ * @param {import("next").NextApiResponse} res
+ */
 export default (req, res) => {
+    var optionalqp = {}
+    if (req.query.replyto) optionalqp.replyto = req.query.replyto
     if(req.method =="POST"){
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
         //console.log(req)
         //JSON.stringify(req.body)
-        fetch(`https://api.aperii.com/users/${req.query.id}/posts`, {
+        fetch(`https://api.aperii.com/users/${req.query.id}/posts${Object.keys(optionalqp).length > 1 ? '?' + new URLSearchParams(optionalqp) : ''}`, {
             method: 'POST',
             body: JSON.stringify(req.body),
             headers: {
