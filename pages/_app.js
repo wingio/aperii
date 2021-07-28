d3df8d4ca5fee114290e1b40b9bfaae3e08d47e7
wingio <mcprowing@gmail.com>
2021-07-28 15:50:06 -0400

    serviceworker

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,16 @@ import styles from '../styles/globals.css'
 import Head from 'next/head'
 import Search from '../components/Search'
 import * as info from '../info.json'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if("serviceWorker" in navigator){
+      navigator.serviceWorker.register('/sw.js').then(r => {
+        console.log("Registered with scope: " + r.scope)
+      }, e => {console.log("Error registering")})
+    }
+  }, [])
   return (<div>
     <Component {...pageProps} />
   </div>)
