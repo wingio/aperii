b350a5b88a6061f30917e758f89553d758f2785a
wingio <mcprowing@gmail.com>
2022-01-23 17:41:57 -0500

    oops

--- api/src/Config.ts
+++ api/src/Config.ts
@@ -1,6 +1,7 @@
+import { config } from 'dotenv';
+config();
 export default class Config {
     public static MONGO_URI: string = "mongodb://localhost:27017/";
     public static DB_NAME: string = "aperii";
-    public static JWT_SECRET: string = "dgbw76xcAW&D6tyaDd66^D^&*^&wyvfufyvteksyvtfGtuykjefdyFYFfftffcffvff";
-    public static HASH_SECRET: string = "ey7rfuyhgfbuygvcfgytusdegu76tft76icatiitt76f&^TgfiF^&*Df6f67f76if67oiFo6gFGKJF--Kdsfdx$677665@@%JDgd";
+    public static JWT_SECRET: string = process.env.JWT_TOKEN || "this is a secret";
 }
