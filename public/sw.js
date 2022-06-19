ab1d98eb26b2e7f9bd09b2a57eea6c277289ca25
wingio <mcprowing@gmail.com>
2022-06-19 12:29:05 -0400

    Fix: CSS not loading due to outdated cache

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,9 @@
 // Choose a cache name
 const cacheName = 'cache-v1';
-// List the files to precache
-const precacheResources = ['/', '/av.png', '/login', '/signup'];
 
 // When the service worker is installing, open the cache and add the precache resources to it
 self.addEventListener('install', (event) => {
   console.log('Service worker install event!');
-  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
 });
 
 self.addEventListener('activate', (event) => {
