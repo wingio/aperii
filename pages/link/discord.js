6f61ea9a4e6c61a853e412b3fe735a8007156bad
wingio <wingio@users.noreply.github.com>
2022-08-12 16:35:40 -0400

    Add discord connection

---
+++ pages/link/discord.js
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router"
+import { API_BASE_URL } from "../../constants"
+
+export default function Discord({ openOauth, successful }) {
+
+    if(typeof window != "undefined"){
+        const router = useRouter()
+        const params = new URL(window.location.href.replace("#", "?")).searchParams;
+
+        if(!params.get("access_token")) {
+            router.push("https://discord.com/oauth2/authorize?client_id=832045938985140244&redirect_uri=http%3A%2F%2Flocalhost%3A5000%2Flink%2Fdiscord&response_type=token&scope=identify")
+            return null
+        }
+
+        fetch(`${API_BASE_URL}/users/@me/connections`, {
+            method: "PUT",
+            headers: {
+                "authorization": window.localStorage.getItem("token"),
+                "content-type": "application/json"
+            },
+            body: `{"discordToken": "${params.get("access_token")}"}`
+        }).then(res => res.json()).then(data => {
+            if(!data.error) router.push("/home")
+        })
+        
+    }
+    return null
+}
